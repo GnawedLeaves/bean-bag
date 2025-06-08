d1c52efa3d13d4fbed57eea8168080c3e8de291c
Marcel <Marcelyap31@gmail.com>
2025-06-08 21:26:05 +0800

    feat: update theme colors and add new background color variables

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,10 @@
 export const theme = {
-    text: "#333333"
-}
+  text: "#261D18",
+  colorBgRed: "#FF552E",
+  colorBgYellow: "#F2A815",
+  colorBgLightYellow: "#FED170",
+  colorBgVoilet: "#B6ACE",
+  colorBgPink: "#FFC1C6",
+  colorBgGreen: "#ABCF82",
+  colorBg: "#F7EBDB",
+};
