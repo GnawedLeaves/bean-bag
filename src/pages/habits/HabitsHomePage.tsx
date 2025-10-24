78d849ab33c7ca39fe134017b341c8c943d84081
Marcel :) <marcel.yap@ncs.com.sg>
2025-10-24 09:47:12 +0800

    merge again

--- src/pages/habits/HabitsHomePage.tsx
+++ src/pages/habits/HabitsHomePage.tsx
@@ -5,12 +5,7 @@ import { HabitsHomePageContainer } from "./HabitsStyles";
 const HabitsHomePage = () => {
   return (
     <ThemeProvider theme={token}>
-<<<<<<< HEAD
-      <HabitsHomePageContainer></HabitsHomePageContainer>
-      test commit
-=======
       <HabitsHomePageContainer>Habits page</HabitsHomePageContainer>
->>>>>>> 3d756fd040ca5558c3575de5e53979ecd447e667
     </ThemeProvider>
   );
 };
