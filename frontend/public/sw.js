104d5877add5b5fefc25058652253793067a33ea
Marcel <Marcelyap31@gmail.com>
2026-03-17 21:57:25 +0800

    test notif image

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,11 +1,15 @@
-// public/sw.js
 self.addEventListener('push', (event) => {
   const data = event.data ? event.data.json() : { title: 'Hello', body: 'New Message' };
   
+  const options = {
+    body: data.body,
+    // Ensure this path is relative to your domain root (public folder)
+    icon: './newIcon.jpg', 
+    // The badge is the tiny icon shown in the Android status bar
+    badge: './newIcon.jpg', 
+  };
+
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/test.jpg', 
-    })
+    self.registration.showNotification(data.title, options)
   );
 });
