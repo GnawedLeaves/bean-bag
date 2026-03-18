0bf889d25c85509635b0448701ca40e0fd8611df
Marcel :) <marcel.yap@ncs.com.sg>
2026-03-18 14:38:32 +0800

    change the controller to use render instead

--- frontend/src/pages/spotify/utils/SpotifyController.tsx
+++ frontend/src/pages/spotify/utils/SpotifyController.tsx
@@ -27,8 +27,8 @@ export const useSpotifyReviewComments = ({
   onCommentAdded,
   onReviewAdded,
 }: UseSpotifyReviewCommentsProps) => {
-  //   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
-  const API_BASE_URL = "http://localhost:5000";
+  const API_BASE_URL =
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
   const addComment = useCallback(
     async (params: AddCommentParams) => {
